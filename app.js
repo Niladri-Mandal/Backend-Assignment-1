20965ef162ec993d03e17658a9d5c4d3a1052af7
Niladri Mandal <150417571+Niladri-Mandal@users.noreply.github.com>
2024-04-14 21:43:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const { sum, multiply, divide, subtract } = require('./export');
+
+console.log('Sum:', sum(5, 3));
+console.log('Multiply:', multiply(5, 3));
+console.log('Divide:', divide(6, 3));
+console.log('Subtract:', subtract(5, 3));
